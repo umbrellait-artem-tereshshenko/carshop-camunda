d08e8783ecba060c3c1063ea193e72b6d77d9f64
Artem Tereshshenko <artemtereshshenko@MacBook-Pro-Admin.local>
2024-10-30 17:57:03 +0300

    Initial commit

---
+++ src/main/java/com/umbrellait/carshop_camunda/CarshopCamundaApplication.java
@@ -0,0 +1,13 @@
+package com.umbrellait.carshop_camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarshopCamundaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarshopCamundaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umbrellait/carshop_camunda/CarshopCamundaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umbrellait.carshop_camunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarshopCamundaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
